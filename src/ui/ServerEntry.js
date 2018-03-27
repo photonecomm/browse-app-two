6e4c531e64b804a045bcc1716be94b5e4594e4bd
DheerajKumar Pentela <dheerajkumar_p@photonint.com>
2018-03-27 15:46:24 +0530

    update

--- src/ui/ServerEntry.js
+++ src/ui/ServerEntry.js
@@ -4,7 +4,7 @@ import { renderToString } from 'react-dom/server';
 import { commonAPI as app, startServer } from '../util/ExpressJSUtils.js';
 import { configManagerObj } from '../util/ConfigManager.js';
 
-//import App from './components/app/app';
+import {HelloWorld} from './components/helloWorld/helloWorld';
 
 const PORT = configManagerObj.env_config_server_port;
 
@@ -16,7 +16,7 @@ app.get(healthInfoURL, function(req, res) {
 
 const pageURL = '/helloworld';
 app.get(pageURL, function(req, res) {
-	let htmlString = renderToString(<App/>);
+	let htmlString = renderToString(<HelloWorld/>);
 	res.send(htmlString);
 });
 
